508baf1c1f24d7b535f74f5e535788d23fa8f00b
devyansh <devyansh.sehgal@gmail.com>
2024-06-24 13:35:37 +0530

    animate services

--- components/Services.jsx
+++ components/Services.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import services0 from "../public/assets/services/services.svg";
@@ -11,10 +11,37 @@ import forword from "../public/assets/services/forward arrow.svg";
 import popupimg1 from "../public/assets/services/popup-react.svg";
 import popupimg2 from "../public/assets/services/popup-square.svg";
 import { RxCross2 } from "react-icons/rx";
+import { motion } from "framer-motion";
 
 function Services() {
   const [visible, setVisible] = useState(false);
   const container = useRef(null);
+  const [inView, setInView] = useState(false);
+
+  const observerRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setInView(entry.isIntersecting);
+      },
+      {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.3, // Adjust threshold as needed
+      }
+    );
+
+    if (observerRef.current) {
+      observer.observe(observerRef.current);
+    }
+
+    return () => {
+      if (observerRef.current) {
+        observer.unobserve(observerRef.current);
+      }
+    };
+  }, []);
 
   function popup() {
     if (visible) {
@@ -82,7 +109,10 @@ function Services() {
   ];
 
   return (
-    <div className="w-full dark:bg-black z-20 lg:h-[70%] relative flex flex-col items-center  justify-start ">
+    <div
+      className="w-full dark:bg-black z-20 lg:h-[70%] relative flex flex-col items-center justify-start"
+      ref={observerRef}
+    >
       <div className="w-full h-full relative flex flex-col justify-center items-center pt-10 lg:pt-20">
         <div className="w-full h-[6rem] relative content-center">
           <div className="w-full left-0 top-[2.1rem] absolute text-teal-300 lg:text-8xl mac:text-7xl text-[3.5rem] font-normal font-['Heaven'] leading-[64px] flex flex-col items-center justify-center">
@@ -94,18 +124,23 @@ function Services() {
         </div>
       </div>
 
-      <div className="w-full h-full  relative flex items-start gap-5 overflow-x-scroll no-scrollbar my-12 lg:mt-20  snap-x snap-mandatory">
-        <div className="mac:min-w-[308px] min-w-[240px]  lg:min-w-[340px]   mac:h-[542px] sm:h-[420px] lg:h-[600px] ml-[13%] relative  snap-center snap-always ">
+      <div className="w-full h-full relative flex items-start gap-5 overflow-x-scroll no-scrollbar my-12 lg:mt-20 snap-x snap-mandatory">
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: inView ? 1 : 0, y: 0 }}
+          transition={{ delay: 0.65, duration: 0.5 }}
+          className="mac:min-w-[308px] min-w-[240px] lg:min-w-[340px] mac:h-[542px] sm:h-[420px] lg:h-[600px] ml-[13%] relative snap-center snap-always"
+        >
           <Image
             src={services0}
             alt=""
-            className=" relative top-0 rounded-[20px]"
+            className="relative top-0 rounded-[20px]"
           />
-          <div className=" w-full pl-2 md:pl-6 md:pr-[35px] py-10 top-0 absolute rounded-[20px] flex-col justify-center items-start gap-2.5 inline-flex">
+          <div className="w-full pl-2 md:pl-6 md:pr-[35px] py-10 top-0 absolute rounded-[20px] flex-col justify-center items-start gap-2.5 inline-flex">
             <div className="text-zinc-950 text-xs md:text-sm font-normal font-['Inter'] leading-none">
               name
             </div>
-            <div className=" text-zinc-950   text-2xl  md:text-2xl font-bold font-['Inter'] ">
+            <div className="text-zinc-950 text-2xl md:text-2xl font-bold font-['Inter']">
               title
             </div>
             <div className="self-stretch h-4 relative">
@@ -120,29 +155,31 @@ function Services() {
             alt=""
             onClick={popup}
           />
-        </div>
+        </motion.div>
 
-        {cardsinfo.map((item) => {
+        {cardsinfo.map((item, index) => {
           return (
-            <div
+            <motion.div
               key={item.id}
-              className="mac:min-w-[308px] min-w-[240px]  lg:min-w-[340px]   mac:h-[542px] sm:h-[420px] lg:h-[600px] relative  snap-center snap-always "
+              initial={{ opacity: 0, y: 50 }}
+              animate={{ opacity: inView ? 1 : 0, y: 0 }}
+              transition={{ delay: 0.65 + index * 0.2, duration: 0.5 }}
+              className="mac:min-w-[308px] min-w-[240px] lg:min-w-[340px] mac:h-[542px] sm:h-[420px] lg:h-[600px] relative snap-center snap-always"
             >
               <Image
                 src={item.image}
                 alt=""
-                className=" relative top-0 rounded-[20px]"
+                className="relative top-0 rounded-[20px]"
               />
-              <div className=" w-full pl-2 md:pl-6 md:pr-[35px] py-10 top-0 absolute rounded-[20px] flex-col justify-center items-start gap-2.5 inline-flex">
-                <div className="text-zinc-950 text-s md:text-sm font-normal font-['Inter'] ">
+              <div className="w-full pl-2 md:pl-6 md:pr-[35px] py-10 top-0 absolute rounded-[20px] flex-col justify-center items-start gap-2.5 inline-flex">
+                <div className="text-zinc-950 text-xs md:text-sm font-normal font-['Inter']">
                   {item.name}
                 </div>
-                <div className=" text-zinc-950  md:text-2xl font-bold font-['Inter']   text-2xl ">
+                <div className="text-zinc-950 text-2xl md:text-2xl font-bold font-['Inter']">
                   {item.title}
                 </div>
                 <div className="self-stretch h-4 relative">
-                  <div className="  text-zinc-950 text-s md:text-sm font-normal font-['Inter'] flex flex-row gap-[2%] ">
-                    {" "}
+                  <div className="text-zinc-950 text-xs md:text-sm font-normal font-['Inter'] flex flex-row gap-[2%]">
                     <Image className="" src={forword} alt="" /> Explore
                   </div>
                 </div>
@@ -153,67 +190,66 @@ function Services() {
                 alt=""
                 onClick={popup}
               />
-            </div>
+            </motion.div>
           );
         })}
       </div>
 
       <div className="w-[122px] h-11 relative">
-        <button className="w-[122px] h-11  bg-zinc-950 dark:bg-white rounded-[10px] text-center text-white dark:text-black text-xs md:text-sm font-normal font-['Inter'] leading-none  cursor-pointer">
-          {" "}
-          <Link href="/services"> Explore all</Link>{" "}
+        <button className="w-[122px] h-11 bg-zinc-950 dark:bg-white rounded-[10px] text-center text-white dark:text-black text-xs md:text-sm font-normal font-['Inter'] leading-none cursor-pointer">
+          <Link href="/services"> Explore all</Link>
         </button>
       </div>
 
       <div
-        className="h-screen overflow-y-scroll no-scrollbar  fixed top-0 z-[100] fill-transparent  flex flex-col items-center w-full p-4 backdrop-blur-lg invisible"
+        className="h-screen overflow-y-scroll no-scrollbar fixed top-0 z-[100] fill-transparent flex flex-col items-center w-full p-4 backdrop-blur-lg invisible"
         ref={container}
       >
-        <div className="rounded-md flex flex-col  mac:w-[74%] lg:w-[74%] w-[94%] backdrop-blur-xl bg-white z-50  lg:p-20 gap-3 mac:p-20 p-8 my-20">
-          <div
-            className="sticky z-52 top-20  flex items-end justify-end cursor-pointer"
+        <div className="rounded-md flex flex-col mac:w-[74%] lg:w-[80%] w-[90%] bg-zinc-200 dark:bg-zinc-950 justify-center relative">
+          <div className="md:h-[25rem] p-4 flex md:flex-row flex-col justify-center items-center relative">
+            <div className="md:w-[50%] flex justify-center relative">
+              <Image
+                src={popupimg1}
+                alt=""
+                className="w-full h-full object-contain relative"
+              />
+            </div>
+            <div className="w-[90%] md:w-[50%] justify-center p-4 relative">
+              <div className="text-zinc-950 dark:text-white text-lg font-bold font-['Inter']">
+                React 18
+              </div>
+              <div className="text-zinc-950 dark:text-white text-xs md:text-sm font-normal font-['Inter']">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s.
+              </div>
+            </div>
+          </div>
+          <div className="h-[25rem] p-4 flex md:flex-row flex-col justify-center items-center relative">
+            <div className="md:w-[50%] justify-center relative">
+              <Image
+                src={popupimg2}
+                alt=""
+                className="w-full h-full object-contain relative"
+              />
+            </div>
+            <div className="w-[90%] md:w-[50%] justify-center p-4 relative">
+              <div className="text-zinc-950 dark:text-white text-lg font-bold font-['Inter']">
+                square 18
+              </div>
+              <div className="text-zinc-950 dark:text-white text-xs md:text-sm font-normal font-['Inter']">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s.
+              </div>
+            </div>
+          </div>
+          <button
+            className="w-[30px] h-[30px] right-2 top-2 absolute justify-center items-center"
             onClick={popup}
           >
-            <RxCross2 className="w-8 h-8 relative -top-10 -right-16 sm:-top-16 sm:-right-14" />
-          </div>
-
-          <div className="lg:text-[32px] text-[20px] font-normal font-['Inter'] leading-loose text-left w-full lg:mt-20 mt-5">
-            Lorem ipsum dolor sit amet.
-          </div>
-          <div className="lg:text-[64px] mac:text-[32px] text-[28px] font-normal  font-['Katibeh'] leading-[64px] text-left w-full">
-            Lorem ipsum dolor sit amet.
-          </div>
-          <div className="text-black text-sm font-normal font-['Inter'] leading-normal text-left sm:text-justify ">
-            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Sapiente maiores totam numquam quam officiis vel
-            doloremque cumque dolores eveniet similique! Lorem ipsum, dolor sit
-            amet consectetur adipisicing elit. Sunt porro quas sapiente
-            necessitatibus aliquid quaerat. Sed eaque laudantium tempore
-            distinctio!
-          </div>
-          <div className=" relative w-full ">
-            <Image src={popupimg1} alt="" className=" min-w-[5rem] " />
-          </div>
-          <div className="text-black text-sm font-normal font-['Inter'] leading-normal text-left sm:text-justify">
-            Lorem ipsum dolor sit amet. lorem20 Lorem, ipsum dolor sit amet
-            consectetur adipisicing elit. Expedita dolorem ab ut necessitatibus
-            consequatur atque repudiandae voluptas placeat saepe in. Lorem
-            ipsum, dolor sit amet consectetur adipisicing elit. Sunt porro quas
-            sapiente necessitatibus aliquid quaerat. Sed eaque laudantium
-            tempore distinctio!
-          </div>
-          <div className="grid grid-cols-2 grid-rows-1 gap-4 w-full mac:p-8 lg:p-8">
-            <Image src={popupimg2} alt="" className="min-w-[5rem]  " />
-            <Image src={popupimg2} alt="" className="min-w-[5rem]   " />
-          </div>
-          <div className="text-black text-sm font-normal font-['Inter'] leading-normal text-left sm:text-justify">
-            Lorem ipsum dolor sit amet.lorem20 Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Placeat exercitationem quidem tempore.
-            Doloribus ex adipisci non corrupti aliquid consectetur sint. Lorem
-            ipsum, dolor sit amet consectetur adipisicing elit. Sunt porro quas
-            sapiente necessitatibus aliquid quaerat. Sed eaque laudantium
-            tempore distinctio!
-          </div>
+            <RxCross2 className="text-black dark:text-white w-full h-full" />
+          </button>
         </div>
       </div>
     </div>
