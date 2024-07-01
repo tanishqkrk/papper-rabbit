4195ec8276e0b66157006ebdce6114b5057c8ee7
devyansh <devyansh.sehgal@gmail.com>
2024-07-01 12:51:57 +0530

    threshold

--- components/GlobalTraction.jsx
+++ components/GlobalTraction.jsx
@@ -3,10 +3,10 @@ import GlobaltractionCard from "./GlobalTractionCard";
 import data from "./data.json";
 
 function GlobalTraction() {
-  const [one, two, three, four] = data.counts;
+  const [projects, startups, countries, years] = data.counts;
 
   return (
-    <div className=" w-full dark:bg-black bg:white  lg:mx-auto flex flex-col justify-between items-center relative pt-10">
+    <div className="w-full dark:bg-black bg-white lg:mx-auto flex flex-col justify-between items-center relative pt-10">
       <div className="w-full h-full relative flex flex-col justify-center items-center">
         <div className="w-full h-[6rem] relative content-center">
           <div className="w-full left-0 top-[2.1rem] absolute text-red-200 lg:text-8xl mac:text-7xl text-[3.5rem] font-normal font-['Heaven'] leading-[64px] flex flex-col items-center justify-center">
@@ -24,7 +24,13 @@ function GlobalTraction() {
             Projects
           </div>
           <div className="text-center">
-            <GlobaltractionCard data={one} id={0} />
+            <GlobaltractionCard
+              data={{
+                id: projects.id,
+                number: parseInt(projects.number),
+                duration: parseInt(projects.duration),
+              }}
+            />
           </div>
         </div>
         <div className="relative flex flex-col items-center justify-center w-[131px] h-[155px] lg:mx-[4rem] mac:mx-[1rem]">
@@ -32,7 +38,13 @@ function GlobalTraction() {
             StartUps
           </div>
           <div className="text-center">
-            <GlobaltractionCard data={two} id={1} />
+            <GlobaltractionCard
+              data={{
+                id: startups.id,
+                number: parseInt(startups.number),
+                duration: parseInt(startups.duration),
+              }}
+            />
           </div>
         </div>
         <div className="relative flex flex-col items-center justify-center w-[154px] h-[155px] lg:mx-[4rem] mac:mx-[1rem]">
@@ -40,7 +52,13 @@ function GlobalTraction() {
             Countries
           </div>
           <div className="text-center">
-            <GlobaltractionCard data={three} id={2} />
+            <GlobaltractionCard
+              data={{
+                id: countries.id,
+                number: parseInt(countries.number),
+                duration: parseInt(countries.duration),
+              }}
+            />
           </div>
         </div>
         <div className="relative flex flex-col items-center justify-center w-[120px] h-[155px] lg:mx-[4rem] mac:mx-[1rem]">
@@ -48,7 +66,13 @@ function GlobalTraction() {
             Years
           </div>
           <div className="text-center">
-            <GlobaltractionCard data={four} id={3} />
+            <GlobaltractionCard
+              data={{
+                id: years.id,
+                number: parseInt(years.number),
+                duration: parseInt(years.duration),
+              }}
+            />
           </div>
         </div>
       </div>
