f6636bf722b99111bfb0832233cac8e5982f1d47
devyansh <devyansh.sehgal@gmail.com>
2024-07-17 17:09:36 +0530

    revamp hutch page; remove 2 useless components

--- app/hutch/page.jsx
+++ app/hutch/page.jsx
@@ -1,24 +1,19 @@
+import React from "react";
 import BackBar from "@/components/back/BackBar";
 import HutchPageCards from "@/components/hutch/HutchPageCards";
-import HutchPageCards2 from "@/components/hutch/HutchPageCards2";
 
 function Hutch() {
   return (
     <div className="w-full flex flex-col items-center justify-center place-content mt-10">
       <BackBar name={"Hutch"} />
       <div className="lg:w-[74%] gap-8 sm:gap-2 items-center justify-center mac:max-w-[80%] w-full grid lg:grid-cols-4 lg:grid-rows-2 mac:grid-cols-4 mac:grid-rows-4 md:grid-cols-2 grid-cols-2 mt-20">
-        <HutchPageCards />
-        <HutchPageCards2 />
-        <HutchPageCards />
-        <HutchPageCards2 />
-        <HutchPageCards />
-        <HutchPageCards2 />
-        <HutchPageCards />
-        <HutchPageCards2 />
-        <HutchPageCards />
-        <HutchPageCards2 />
-        <HutchPageCards />
-        <HutchPageCards2 />
+        {[...Array(12)].map((_, index) => (
+          <HutchPageCards
+            key={index}
+            src={`/assets/hutchimg/img${index % 2 === 0 ? 1 : 2}.png`}
+            varValue={index % 2 === 0 ? 0 : 40}
+          />
+        ))}
       </div>
     </div>
   );
--- components/hutch/HutchPageCards.jsx
+++ components/hutch/HutchPageCards.jsx
@@ -1,28 +1,40 @@
 import React from "react";
-import services0 from "../../public/assets/hutchimg/img2.png";
 import Image from "next/image";
 
-function HutchPageCards() {
+const HutchPageCards = ({ src, varValue }) => {
   return (
-    <div className="min-w-[240px] max-w-[360px] relative  mx-auto z-20 mt-20 sm:min-w-[150px] ">
-      <Image
-        className="w-full relative -z-20 rounded-[20px]"
-        src={services0}
-        alt=""
-      />
+    <div
+      className={`min-w-[240px] max-w-[360px] relative mx-auto z-20 mt-[${varValue}] sm:min-w-[150px]`}
+    >
+      <div
+        style={{
+          position: "relative",
+          width: "100%",
+          height: "100%",
+          zIndex: "-20",
+        }}
+      >
+        <Image
+          className="w-full relative -z-20 rounded-[20px]"
+          src={src}
+          alt=""
+          width={360}
+          height={220}
+        />
+      </div>
 
-      <div className="h-[220px] sm:h-[180px] m-1  absolute bottom-0 rounded-[20px] flex flex-col justify-center items-center gap-2 group/items ">
-        <div className=" h-[100%] m-2 sm:m-1 relative rounded-[20px] flex flex-col justify-center items-center gap-2 p-2 ">
+      <div className="h-[220px] sm:h-[180px] m-1 absolute bottom-0 rounded-[20px] flex flex-col justify-center items-center gap-2 group/items">
+        <div className="h-[100%] m-2 sm:m-1 relative rounded-[20px] flex flex-col justify-center items-center gap-2 p-2">
           <div className="w-full h-full absolute -z-10 bg-gray-50 bg-opacity-20 rounded-2xl backdrop-blur-[20px]" />
-          <div className="flex items-center justify-center text-black lg:text-2xl mac:text-[1.1rem] font-bold font-['Inter'] text-xs ">
+          <div className="flex items-center justify-center text-black lg:text-2xl mac:text-[1.1rem] font-bold font-['Inter'] text-xs">
             Perspiciatis unde omrs
           </div>
-          <div className="text-justify text-black text-xs font-normal relative font-['Inter'] lg:leading-normal sm:leading-none">
+          <div className="text-justify text-black text-xs font-normal relative font-['Inter'] leading-normal sm:leading-none">
             &quot;Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
             do eiusmod tempor incididunt ut labore et dolore magna aliqua.&quot;
           </div>
           <div className="w-[122px] relative">
-            <button className="w-[122px]  lg:h-11 mac:h-11 h-8  bg-zinc-950 rounded-[10px] text-center text-white text-xs md:text-sm font-normal font-['Inter'] leading-none ">
+            <button className="w-[122px] lg:h-11 mac:h-11 h-8 bg-zinc-950 rounded-[10px] text-center text-white text-xs md:text-sm font-normal font-['Inter'] leading-none">
               Learn
             </button>{" "}
           </div>
@@ -30,6 +42,6 @@ function HutchPageCards() {
       </div>
     </div>
   );
-}
+};
 
 export default HutchPageCards;
